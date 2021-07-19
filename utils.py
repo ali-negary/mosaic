4f2ac2d8ad8a613e7f482bf597fdb373500284eb
Ali Negary <ali.negary@outlook.com>
2021-07-19 13:35:04 +0430

    wrong file deleted

--- utils.py
+++ utils.py
@@ -26,20 +26,22 @@ def check_weights(tests, marks):
 
 # This method calculates the average of students grades and maybe construct the json output.
 def calculate_grades(courses, students, tests, marks):
+    print("___________________marks_tests________________________")
     marks_tests = pd.merge(marks, tests, how='left', on="test_id")
     # This gives us the weighted marks for each student.
     marks_tests['weighted_marks'] = marks_tests['test_mark'] * marks_tests['test_weight'] / 100
     print(marks_tests)
-    print("___________________________________________")
+    print("___________________sum_of_all________________________")
     # This gives us the sum of all courses for each student.
     sum_of_all = marks_tests.groupby(['student_id']).sum()
     print(sum_of_all)
-    print("___________________________________________")
+    print("_____________________sum_per_course______________________")
     sum_per_course = marks_tests.groupby(['student_id', 'course_id']).sum()
     print(sum_per_course)
-    print("___________________________________________")
+    print("____________________sum_per_course_______________________")
     sum_per_course = pd.merge(sum_per_course, courses, how='left', on="course_id")
     print(sum_per_course)
-    print("___________________________________________")
+    print("_______________________________JSON XXX_______________________________")
+    print(sum_per_course.to_json(orient='records'))
     results = {}
     return results
