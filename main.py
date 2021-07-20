cf448b576b86e72a04dc4c2d917ab0c21393fb56
Ali Negary <ali.negary@outlook.com>
2021-07-21 00:48:43 +0430

    csv2json created. works correctly.

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ import utils
 import sys
 
 if __name__ == '__main__':
+
     files_list = sys.argv
-    # print(files_list)
+
     students_df = utils.read_file(files_list[2], ['student_id', 'student_name'])
     courses_df = utils.read_file(files_list[1], ['course_id', 'course_name', 'teacher_name'])
     test_df = utils.read_file(files_list[3], ['test_id', 'course_id', 'test_weight'])
     marks_df = utils.read_file(files_list[4], ['test_id', 'student_id', 'test_mark'])
+
     if utils.check_weights(test_df, marks_df):
-        result = utils.calculate_grades(courses=courses_df, students=students_df, tests=test_df, marks=marks_df)
+
+        info_and_grades = utils.calculate_grades(courses=courses_df, students=students_df, tests=test_df, marks=marks_df)
+
+        result = utils.csv2json(info_and_grades)
     else:
         result = {"error": "Invalid course weights"}
 
--- utils.py
+++ utils.py
@@ -24,24 +24,56 @@ def check_weights(tests, marks):
     return (marks_tests['test_weight'] == 100).all()
 
 
-# This method calculates the average of students grades and maybe construct the json output.
+# This method calculates the average of students grades.
 def calculate_grades(courses, students, tests, marks):
     print("___________________marks_tests________________________")
     marks_tests = pd.merge(marks, tests, how='left', on="test_id")
     # This gives us the weighted marks for each student.
     marks_tests['weighted_marks'] = marks_tests['test_mark'] * marks_tests['test_weight'] / 100
     print(marks_tests)
-    print("___________________sum_of_all________________________")
-    # This gives us the sum of all courses for each student.
+    print("___________________students_average________________________")
+    # This gives us the average of all courses for each student.
     sum_of_all = marks_tests.groupby(['student_id']).sum()
-    print(sum_of_all)
+    sum_of_all['student_average'] = (sum_of_all['weighted_marks'] / sum_of_all['test_weight'] * 100).round(2)
+    students_average = sum_of_all.drop(labels=['test_id', 'test_mark', 'course_id', 'weighted_marks', 'test_weight'], axis=1).reset_index()
+    print(students_average)
     print("_____________________sum_per_course______________________")
-    sum_per_course = marks_tests.groupby(['student_id', 'course_id']).sum()
-    print(sum_per_course)
-    print("____________________sum_per_course_______________________")
+    sum_per_course = marks_tests.groupby(['student_id', 'course_id']).sum().reset_index()
     sum_per_course = pd.merge(sum_per_course, courses, how='left', on="course_id")
-    print(sum_per_course)
-    print("_______________________________JSON XXX_______________________________")
-    print(sum_per_course.to_json(orient='records'))
-    results = {}
-    return results
+    complete_result = pd.merge(sum_per_course, students, how='left', on="student_id").drop(labels=['test_id', 'test_mark', 'test_weight'], axis=1)
+    print(complete_result)
+    print("____________________________complete_complete______________________________")
+    complete_complete = pd.merge(complete_result, students_average, how='left', on="student_id")
+    print(complete_complete)
+    return complete_complete
+
+
+def csv2json(complete_dataset):
+    results = complete_dataset.to_dict('records')
+    final_details = {"students": []}
+    records = dict()
+    for student in results:
+        temp = dict()
+        if student['student_id'] not in records.keys():
+            temp["id"] = student['student_id']
+            temp["name"] = student['student_name']
+            temp["totalAverage"] = round(student['student_average'], 2)
+            temp['courses'] = [{
+                "id": student['course_id'],
+                "name": student['course_name'],
+                "teacher": student['teacher_name'],
+                "courseAverage": round(student['weighted_marks'], 2)
+            }]
+            records[student['student_id']] = temp
+        else:
+            records[student['student_id']]['courses'].append({
+                "id": student['course_id'],
+                "name": student['course_name'],
+                "teacher": student['teacher_name'],
+                "courseAverage": round(student['weighted_marks'], 2)
+            })
+    final_details['students'] = list(records.values())
+    print("________________________________________students_detail_____________________________________")
+    print(final_details)
+
+    return final_details
