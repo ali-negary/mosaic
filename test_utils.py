16c9a7953876a86400fd8beb305d1c591317a644
Ali Negary <ali.negary@outlook.com>
2021-07-22 01:54:31 +0430

    added unit test for read_file method.

---
+++ test_utils.py
@@ -0,0 +1,16 @@
+import unittest
+import utils
+
+
+class TestUtil(unittest.TestCase):
+
+    def test_read_file(self):
+        # normal situation
+        correct_result0 = utils.read_file('test_cases/correct_test_read_file0.csv', ['student_id', 'student_name'])
+        self.assertTrue(type(correct_result0), "<class 'pandas.core.frame.DataFrame'>")
+        self.assertTrue(list(correct_result0.columns), ['student_id', 'student_name'])
+        # file name is .txt instead of .csv
+        correct_result1 = utils.read_file('test_cases/correct_test_read_file1.txt', ['student_id', 'student_name'])
+        self.assertTrue(type(correct_result1), "<class 'pandas.core.frame.DataFrame'>")
+        self.assertTrue(list(correct_result1.columns), ['student_id', 'student_name'])
+
