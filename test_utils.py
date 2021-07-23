7a131957158261a395bef5b2a985822fbaa5564d
Ali Negary <ali.negary@outlook.com>
2021-07-23 23:42:36 +0430

    unit test for csv2json method.

--- test_utils.py
+++ test_utils.py
@@ -17,7 +17,20 @@ class TestUtil(unittest.TestCase):
         # the columns are not enough
         with self.assertRaises(Exception):
             utils.read_file('test_cases/incorrect_test_read_file0.txt', ['student_id', 'student_name'])
+            utils.read_file('test_cases/incorrect_test_read_file1.csv')
 
-
-
-
+    def test_csv2json(self):
+        # normal situation
+        correct_case = pandas.read_csv('test_cases/correct_test_csv2json0.csv', index_col=0)
+        for student in utils.csv2json(correct_case)['students']:
+            self.assertTrue(list(student.keys()), ['id', 'name', 'totalAverage', 'courses'])
+            self.assertGreater(len(student['courses']), 0)
+            self.assertTrue(student['totalAverage'], float)
+            for course in student['courses']:
+                self.assertTrue(course['courseAverage'], float)
+        #  values are nan (empty indexed rows) - I wrote this really quick. so this might not be a good test.
+        misshaped_case = pandas.read_csv('test_cases/misshaped_test_csv2json0.csv', index_col=0)
+        for student in utils.csv2json(misshaped_case)['students']:
+            self.assertTrue(list(student.keys()), ['id', 'name', 'totalAverage', 'courses'])
+            self.assertGreater(len(student['courses']), 0)
+            self.assertTrue(student['totalAverage'], float)
